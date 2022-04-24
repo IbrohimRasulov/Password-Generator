056fdf4f10af5b25cd4bd2b7268b267dcca695a1
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-04-24 05:48:27 +0500

    Add a function to show live input-range value

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const buttons = document.querySelectorAll('.password-btn');
+const inputRange = document.querySelector('#slider');
+const passwordLength = document.querySelector('#input-range p span');
+
+inputRange.addEventListener('input', () => {
+  const newValue = inputRange.value;
+  passwordLength.textContent = newValue;
+});
+
+
+console.log(inputRange);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 buttons.forEach(button => {
   button.innerHTML = '<img src="assets/Icon-dots.png" alt="Icon-dots">';
