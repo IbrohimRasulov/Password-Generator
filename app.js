2c6d34694c0d2287b948f31e4a2e539283e3e415
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-04-24 17:57:42 +0500

    Style slider and add animation to slider valur

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const buttons = document.querySelectorAll('.password-btn');
-const inputRange = document.querySelector('#slider');
 const passwordLength = document.querySelector('#input-range p span');
 const symbolsCheckbox = document.querySelector('#symbols');
 const lettersCheckbox = document.querySelector('#letters');
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const generatorBtn = document.querySelector('#generator-btn button');
-const popUp = document.querySelector('#notification')
+const popUp = document.querySelector('#notification');
+const rangeInput = document.querySelector('#slider');
+const rangeValue = document.querySelector('#slider-value div');
 
 let charset;
 let password = "";
 
 buttons.forEach(button => {
   button.innerHTML = '<img src="assets/Icon-dots.png" alt="Icon-dots">';
-});
-
-inputRange.addEventListener('input', () => {
-  const newValue = inputRange.value;
-  passwordLength.textContent = newValue;
 });
 
 checkboxes.forEach(checkbox => {
@@ -62,9 +58,22 @@ buttons.forEach(button => {
 
       popUp.setAttribute('id', '');
 
-      setTimeout(function() {
+      setTimeout(function () {
         popUp.setAttribute('id', 'notification');
       }, 3000)
     }
   });
+});
+
+let start = parseFloat(rangeInput.min);
+let end = parseFloat(rangeInput.max);
+let step = parseFloat(rangeInput.step);
+
+for (let i = start; i <= end; i += step) {
+  rangeValue.innerHTML += '<div>' + i + '</div>';
+}
+
+rangeInput.addEventListener("input", function () {
+  let top = parseFloat(rangeInput.value) / step * -30;
+  rangeValue.style.marginTop = top + "px";
 });
