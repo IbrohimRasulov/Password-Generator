718c7daaf72a27b24221aaaea397cb0a717011d9
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-04-24 08:54:24 +0500

    Add eventlisteners to checkboxes to toggle chencked attribute

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const buttons = document.querySelectorAll('.password-btn');
 const inputRange = document.querySelector('#slider');
 const passwordLength = document.querySelector('#input-range p span');
+const symbolsCheckbox = document.querySelector('#symbols');
+const lettersCheckbox = document.querySelector('#letters');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 inputRange.addEventListener('input', () => {
   const newValue = inputRange.value;
   passwordLength.textContent = newValue;
 });
 
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('click', () => {
+    checkbox.toggleAttribute('checked');
+    console.log('clicked');
+  });
+});
 
-console.log(inputRange);
+let charset = "";
+
+switch (true) {
+  case (symbolsCheckbox.checked && lettersCheckbox.checked):
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~-=`|{}[]:;?<>,./";
+    break;
+  case (symbolsCheckbox.checked):
+    charset = "0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    break;
+  case (lettersCheckbox.checked):
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    break
+  default:
+    charset = "0123456789";
+    break;
+}
+
+
+
+
+// console.log(checkboxes);
 
 
 
